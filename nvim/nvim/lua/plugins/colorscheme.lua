42f0dda4c872a0f1209b9da6093ef0717922ad3a
Hakim Saoudi <saoudihakim@gmail.com>
2025-03-01 02:24:37 +0100

    feat(nvim): add nvim

---
+++ nvim/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,117 @@
+-- return {
+--   {
+--     "craftzdog/solarized-osaka.nvim",
+--     branch = "osaka",
+--     lazy = true,
+--     priority = 1000,
+--     opts = function()
+--       return {
+--         transparent = false,
+--       }
+--     end,
+--   },
+--
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "solarized-osaka",
+--     },
+--   },
+-- }
+
+return {
+  -- add tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      style = "night",
+    },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
+
+--   {
+--     "catppuccin/nvim",
+--     name = "catppuccin",
+--     priority = 1000,
+--     build = ":CatppuccinCompile",
+--     opts = {
+--       -- compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
+--       flavour = "mocha",
+--       -- transparent_background = not vim.g.neovide,
+--       term_colors = true,
+--       -- custom_highlights = require("plugins.ui.theme.override"),
+--       dim_inactive = {
+--         enabled = false,
+--         shade = "dark",
+--         percentage = 0.15,
+--       },
+--       integrations = {
+--         blink_cmp = false,
+--         dap = true,
+--         dap_ui = true,
+--         dashboard = true,
+--         flash = true,
+--         fzf = true,
+--         notify = true,
+--         noice = true,
+--         neotree = false,
+--         gitsigns = true,
+--         markdown = true,
+--         headlines = true,
+--         telekasten = true,
+--         ts_rainbow2 = false,
+--         lsp_trouble = false,
+--         native_lsp = {
+--           enabled = true,
+--           virtual_text = {
+--             errors = { "italic" },
+--             hints = { "italic" },
+--             warnings = { "italic" },
+--             information = { "italic" },
+--           },
+--           underlines = {
+--             errors = { "underline" },
+--             hints = { "underline" },
+--             warnings = { "underline" },
+--             information = { "underline" },
+--           },
+--           inlay_hints = {
+--             background = true,
+--           },
+--         },
+--         rainbow_delimiters = true,
+--         treesitter = true,
+--         treesitter_context = false,
+--         telescope = {
+--           enabled = true,
+--         },
+--
+--         navic = { enabled = false },
+--         -- leap               = true,
+--         mason = false,
+--         indent_blankline = {
+--           enable = true,
+--           colored_indent_levels = true,
+--         },
+--         window_picker = false,
+--         which_key = false,
+--         symbols_outline = false,
+--       },
+--     },
+--   },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "catppuccin",
+--     },
+--   },
+-- }
---
+++ nvim/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      explorer = {
+        -- your explorer configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        -- show hidden files
+      },
+      picker = {
+        sources = {
+          explorer = {
+            -- your explorer picker configuration comes here
+            -- or leave it empty to use the default settings
+            hidden = true,
+            ignored = true,
+          },
+        },
+      },
+    },
+  },
+}
